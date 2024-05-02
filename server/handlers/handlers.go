1c6f2474d01798a56793051769772d6a00f7e6de
qwark97 <marcin.plata1@gmail.com>
2024-05-02 22:10:27 +0200

    clean up handlers

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -11,25 +11,24 @@ import (
 	"github.com/vargspjut/wlog"
 
 	"github.com/gorilla/mux"
-	"github.com/gorilla/websocket"
-	"github.com/qwark97/assistant/llms/openai"
-	"github.com/qwark97/assistant/server/controller"
+	"github.com/qwark97/assistant/server/agent"
 	"github.com/qwark97/assistant/server/model"
 	"github.com/qwark97/assistant/server/storage/data"
 )
 
 type Server struct {
 	router *mux.Router
-	cont   controller.Controller
+	agent  agent.Agent
 	log    wlog.Logger
 	env    map[string]string
 }
 
 func NewServer(router *mux.Router, env map[string]string, log wlog.Logger) Server {
-	cont := controller.New(data.New(), log)
+	d := data.New()
+	agent := agent.New(d, log)
 	return Server{
 		router: router,
-		cont:   cont,
+		agent:  agent,
 		log:    log,
 		env:    env,
 	}
@@ -43,10 +42,10 @@ func (s Server) RegisterRoutes() error {
 }
 
 func (s Server) interaction(w http.ResponseWriter, r *http.Request) {
+	s.log.Debug("received")
 	ctx, cancel := context.WithTimeout(r.Context(), time.Minute*5)
 	defer cancel()
 
-	s.log.Debug("received")
 	w.Header().Add("Content-Type", "application/json")
 
 	var data model.InteractionRequest
@@ -60,80 +59,79 @@ func (s Server) interaction(w http.ResponseWriter, r *http.Request) {
 		data.ConversationID = uuid.New()
 	}
 
-	llm := openai.New(s.env["OPENAI_KEY"], s.log)
+	response := s.agent.Interact(ctx, data)
 
-	response := s.cont.Interact(ctx, data, llm)
-
-	res := map[string]string{
-		"response":       response,
-		"converation_id": data.ConversationID.String(),
+	res := model.InteractionResponse{
+		Answer:         response,
+		ConversationID: data.ConversationID,
 	}
 	if err := json.NewEncoder(w).Encode(res); err != nil {
 		s.log.Error(fmt.Sprintf("encode: %s", err.Error()))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	s.log.Debug("respond")
 }
 
 func (s Server) chat(w http.ResponseWriter, r *http.Request) {
-	var upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-	s.log.Infof("received %s request", r.Method)
+	// var upgrader = websocket.Upgrader{
+	// 	ReadBufferSize:  1024,
+	// 	WriteBufferSize: 1024,
+	// 	CheckOrigin: func(r *http.Request) bool {
+	// 		return true
+	// 	},
+	// }
+	// s.log.Infof("received %s request", r.Method)
 
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		s.log.Error(fmt.Sprintf("upgrade: %s", err.Error()))
-		return
-	}
-	defer conn.Close()
+	// conn, err := upgrader.Upgrade(w, r, nil)
+	// if err != nil {
+	// 	s.log.Error(fmt.Sprintf("upgrade: %s", err.Error()))
+	// 	return
+	// }
+	// defer conn.Close()
 
-	llm := openai.New(s.env["OPENAI_KEY"], s.log)
-	ctx := r.Context()
+	// llm := openai.New(s.env["OPENAI_KEY"], s.log)
+	// ctx := r.Context()
 
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			if websocket.IsCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-				s.log.Info("closing connection")
-			} else {
-				s.log.Errorf("failed to read the message: %s", err.Error())
-			}
-			return
-		}
-		if messageType != websocket.TextMessage {
-			if err := conn.WriteMessage(messageType, []byte("Sorry, I can't handle this type of message")); err != nil {
-				s.log.Errorf("failed to write the message: %s", err.Error())
-				return
-			}
-		}
+	// for {
+	// 	messageType, p, err := conn.ReadMessage()
+	// 	if err != nil {
+	// 		if websocket.IsCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+	// 			s.log.Info("closing connection")
+	// 		} else {
+	// 			s.log.Errorf("failed to read the message: %s", err.Error())
+	// 		}
+	// 		return
+	// 	}
+	// 	if messageType != websocket.TextMessage {
+	// 		if err := conn.WriteMessage(messageType, []byte("Sorry, I can't handle this type of message")); err != nil {
+	// 			s.log.Errorf("failed to write the message: %s", err.Error())
+	// 			return
+	// 		}
+	// 	}
 
-		var data model.InteractionRequest
-		err = json.Unmarshal(p, &data)
-		if err != nil {
-			s.log.Errorf("failed to decode the message: %s", err.Error())
-			return
-		}
+	// 	var data model.InteractionRequest
+	// 	err = json.Unmarshal(p, &data)
+	// 	if err != nil {
+	// 		s.log.Errorf("failed to decode the message: %s", err.Error())
+	// 		return
+	// 	}
 
-		response := s.cont.Interact(ctx, data, llm)
+	// 	response := s.agent.Interact(ctx, data)
 
-		res := map[string]string{
-			"response":       response,
-			"converation_id": data.ConversationID.String(),
-		}
-		r, err := json.Marshal(res)
-		if err != nil {
-			s.log.Errorf("failed to encode the response message: %s", err.Error())
-			return
-		}
+	// 	res := map[string]string{
+	// 		"response":       response,
+	// 		"converation_id": data.ConversationID.String(),
+	// 	}
+	// 	r, err := json.Marshal(res)
+	// 	if err != nil {
+	// 		s.log.Errorf("failed to encode the response message: %s", err.Error())
+	// 		return
+	// 	}
 
-		if err := conn.WriteMessage(messageType, r); err != nil {
-			s.log.Errorf("failed to write the response message: %s", err.Error())
-			return
-		}
-	}
+	// 	if err := conn.WriteMessage(messageType, r); err != nil {
+	// 		s.log.Errorf("failed to write the response message: %s", err.Error())
+	// 		return
+	// 	}
+	// }
 }
